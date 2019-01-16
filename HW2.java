5a1aa0b4acbc942f7cec7934a74e58455b37ce2c
aaljanabi <46700928+aaljanabi@users.noreply.github.com>
2019-01-16 14:05:11 -0800

    Add files via upload

---
+++ HW2.java
@@ -0,0 +1,23 @@
+
+public class HW2 {
+
+	public static void main(String[] args) 
+	{{int first=1,second=1,next,i;
+	System.out.print(first+" "+second+" ");
+	for(i=3;i<=12;i++)
+	{next=first+second;
+	System.out.print(next+"");
+	first=second;
+	second=next;
+	}
+	}
+	
+		
+	}
+	
+
+
+
+		
+	}
+
